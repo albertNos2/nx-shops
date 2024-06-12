ead2ee09a3d945e63e0b1e9048dbc23ca086f523
Noswitz <Albert.Noswitz@partner.bmw.de>
2024-06-12 11:08:06 +0200

    move cypress files

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
-    //supportFile: false,
+    supportFile: './e2e-ci/cypress/support/e2e.ts',
+    specPattern: './e2e-ci/cypress/e2e/**/*.cy.ts',
   },
 });
